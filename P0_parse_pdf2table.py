70e34588017ef5af4db6aa8692819709587f089c
thoppe <travis.hoppe@gmail.com>
2023-08-04 12:23:48 -0400

    Add parsing code

---
+++ P0_parse_pdf2table.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import camelot
+
+load_src = Path("data/department_org_src/")
+F_PDF = list(load_src.glob("*.pdf"))
+
+save_dest = Path("data/department_PDF2table")
+save_dest.mkdir(exist_ok=True, parents=True)
+
+for f in F_PDF:
+    tables = camelot.read_pdf(str(f), pages="1-end")
+
+    for k, table in enumerate(tables):
+        print(table.parsing_report)
+        f_save = save_dest / f"{f.stem}_{k:04d}.csv"
+
+        table.df.to_csv(f_save, index=False)
