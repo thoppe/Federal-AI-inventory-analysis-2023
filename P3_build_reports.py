b01959b0b0c4bc041f4bb4aaa6fba47621952727
thoppe <travis.hoppe@gmail.com>
2023-08-08 14:07:35 -0400

    Update Report by sorting

--- P3_build_reports.py
+++ P3_build_reports.py
@@ -21,21 +21,19 @@ doc = []
 doc.append("# AI topics across Departments")
 doc.append("")
 
-for dept, dx in df.groupby("Department"):
-    
+for dept, dx in df.groupby("Department", sort=False):   
     doc.append(f"- [{dept} ({len(dx)})](#{dept})")
 
-'''
+
 doc.append("")
-for dept, dx in df.groupby("Department"):
+for dept, dx in df.groupby("Department", sort=False):
     doc.append(f"## {dept}")
     for line in dx["highlight"]:
         doc.append(f"- {line}")
     doc.append("")
-'''
+
 markdown = "\n".join(doc)
 print(markdown)
-exit()
 
 with open(f_markdown, "w") as FOUT:
     FOUT.write(markdown)
